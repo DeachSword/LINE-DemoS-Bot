a223d9ba46a495a1bf066ce0dd5f8c6f63008168
YinMo <aseekgodroad@gmail.com>
2020-12-14 06:35:52 +0800

    Update main.py

--- api/timeline/main.py
+++ api/timeline/main.py
@@ -32,6 +32,24 @@ class Timeline():
             'x-lsr':'TW', #needless
             'x-u': '' #needless
         })
-        url = self.server.urlEncode('https://legy-jp.line.naver.jp/ext/album', '/api/v4/album/%s' % albumId, params)
+        url = self.server.urlEncode('https://gwz.line.naver.jp/ext/album', '/api/v4/album/%s' % albumId, params)
         r = self.server.postContent(url, headers=hr)
+        return r.json()
+        
+
+    def addImageToAlbum(self, mid, albumId, oid):
+        #oid like 6cbff2e4100006b58db80f87ad8666bc.20121408
+        params = {'homeId': mid}
+        data = json.dumps({"photos":[{"oid": oid}]})
+        
+        hr = self.server.additionalHeaders(self.server.timelineHeaders, {
+            'x-lhm': 'PUT',
+            'content-type': "application/json",
+            'x-album-stats': "e2FsYnVtUGhvdG9BZGRDb3VudD0xfQ==" #change it if you want update many images
+        })
+
+        url = self.server.urlEncode('https://gwz.line.naver.jp/ext/album', '/api/v3/photos/%s' % albumId, params)
+        r = self.server.postContent(url, data=data, headers=hr)
+        
+        #{"code":0,"message":"success","result":true}
         return r.json()
